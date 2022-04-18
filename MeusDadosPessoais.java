bb4af6a942617f713c5eb7b40fd1c9d5094ae1ad
Jessy <jessicamanfiosilva@yahoo.com.br>
2022-04-18 14:11:09 -0300

    first commit

---
+++ MeusDadosPessoais.java
@@ -0,0 +1,23 @@
+class MeusDadosPessoais
+
+
+
+public static void main (String entrada[])}
+
+{
+
+int idade= 36;
+
+String frase = "Jessica Manfio";
+
+
+
+System.out.printLn(" Meu nome é , + frase+ "tenho + inteiro + " anos");
+
+
+
+System.exit(0);
+
+}
+
+}
